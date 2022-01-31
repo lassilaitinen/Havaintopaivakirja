8fe0ba2788f34a6fa23d91cc07ca0da001c1fb0f
loklaiti <lassi.k.laitinen@student.jyu.fi>
2022-01-31 12:35:25 +0200

    Vaihe 2 kuvat

---
+++ paivakirja/src/fxPaivakirja/PaivakirjaGUIController.java
@@ -0,0 +1,10 @@
+package fxPaivakirja;
+
+/**
+ * @author lassi
+ * @version 20.1.2022
+ *
+ */
+public class PaivakirjaGUIController {
+      // TODO
+}
---
+++ paivakirja/src/fxPaivakirja/PaivakirjaMain.java
@@ -0,0 +1,38 @@
+package fxPaivakirja;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+
+
+/**
+ * @author lassi
+ * @version 20.1.2022
+ *
+ */
+public class PaivakirjaMain extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader ldr = new FXMLLoader(getClass().getResource("PaivakirjaGUIView.fxml"));
+            final Pane root = ldr.load();
+            //final PaivakirjaGUIController paivakirjaCtrl = (PaivakirjaGUIController) ldr.getController();
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("paivakirja.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Paivakirja");
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param args Ei käytössä
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
