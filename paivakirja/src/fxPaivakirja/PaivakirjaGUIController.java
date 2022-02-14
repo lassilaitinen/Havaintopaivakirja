8103de6ccaa0eb2399b25b3c780ad07a04526b77
loklaiti <lassi.k.laitinen@student.jyu.fi>
2022-02-14 14:42:29 +0200

    Aloitettu vaihetta 3

--- paivakirja/src/fxPaivakirja/PaivakirjaGUIController.java
+++ paivakirja/src/fxPaivakirja/PaivakirjaGUIController.java
@@ -1,10 +1,83 @@
 package fxPaivakirja;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import fi.jyu.mit.fxgui.Dialogs;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Menu;
+import javafx.scene.control.TextField;
+
+
 /**
  * @author lassi
  * @version 20.1.2022
  *
  */
 public class PaivakirjaGUIController {
-      // TODO
+
+    @FXML
+    private Button buttonPoistaH;
+
+    @FXML
+    private Button buttonPoistaLaji;
+
+    @FXML
+    private Button buttonUusiH;
+
+    @FXML
+    private Button buttonUusiLaji;
+
+    @FXML
+    private ComboBox<?> cboxVEhdot;
+
+    @FXML
+    private Menu menuApua;
+
+    @FXML
+    private Menu menuMuokkaa;
+
+    @FXML
+    private Menu menuTiedosto;
+
+    @FXML
+    private TextField textHae;
+
+    @FXML
+    void mouseClicked() {
+        virheilmoitus();
+    }
+    
+    @FXML
+    void handeApua() {
+        avustus();
+    }
+
+    
+    //--------------------------------------
+    
+    /**
+     * Näytetään ohjelman suunnitelma erillisessä selaimessa.
+     */
+    private void avustus() {
+        Desktop desktop = Desktop.getDesktop();
+        try {
+            URI uri = new URI("https://tim.jyu.fi/view/kurssit/tie/ohj2/2022k/ht/loklaiti");
+            desktop.browse(uri);
+        } catch (URISyntaxException e) {
+            return;
+        } catch (IOException e) {
+            return;
+        }
+    }
+    
+    
+    private void virheilmoitus() {
+        Dialogs.showMessageDialog("Ei toimi vielä!");
+    }
+
 }
